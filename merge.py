ed0caaf9728965a086598eb0c291dcfb218fac2c
coderbijarniya <77825881+coderbijarniya@users.noreply.github.com>
2023-05-18 21:58:56 +0530

    merge.py
    Updated merge.py

--- merge.py
+++ merge.py
@@ -4,46 +4,37 @@ from typing import List
 class Node:
     def __init__(self, name):
         self.name = name
-        self.children = []
+        self.children = {}
 
     def add_child(self, child):
-        self.children.append(child)
+        self.children[child.name] = child
 
     def print(self):
-        queue = [(self, 0)]  
-        # Maintain a queue with node and its depth for BFS traversal
+        queue = [(self, 0)]  # Maintain a queue with node and its depth
 
         while queue:
             node, depth = queue.pop(0)
-            children = ", ".join(child.name for child in node.children) 
-            #Children names added as string to node name string
+            children = ", ".join(node.children.keys())
             print(f"{node.name}: {children}")
-            for child in node.children:
+
+            for child in node.children.values():
                 queue.append((child, depth + 1))
-            #Adding children in queue to go down the depth    
 
 
 def merge(inp: List[List[str]]) -> Node:
     root = Node("*")  # Create a unique root node
-    node_map = {("*",): root}  # Map node-paths to their corresponding Node objects
+    node_map = {("*",): root}  # Map node paths to their corresponding Node objects
 
     for lst in inp:
         current_node = root  # Start at the root node for each list
-        path = [current_node.name]  # Keep track of the current path from the root
 
         for item in lst[1:]:  # Skip the first item which is always "*"
-            path.append(item)
-            node_key = tuple(path)
-
-            if node_key not in node_map:
+            if item not in current_node.children:
                 new_node = Node(item)
-                node_map[node_key] = new_node
                 current_node.add_child(new_node)
                 current_node = new_node
             else:
-                current_node = node_map[node_key]
-
-        path.pop()  # Remove the last item in the path
+                current_node = current_node.children[item]
 
     return root
 
